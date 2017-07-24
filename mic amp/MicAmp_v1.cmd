69b946374a3436f1a490ad620aad953e8d8caab0
Evan Foss <evanfoss@gmail.com>
2017-07-24 17:00:43 -0400

    mic amp included and revisions to the readme files

---
+++ mic amp/MicAmp_v1.cmd
@@ -0,0 +1,133 @@
+# Pin name action command file
+
+# Start of element U1
+ChangePinName(U1, 5, Trim+)
+ChangePinName(U1, 1, Trim+)
+ChangePinName(U1, 4, V-)
+ChangePinName(U1, 7, V+)
+ChangePinName(U1, 6, out)
+ChangePinName(U1, 2, in-)
+ChangePinName(U1, 3, in+)
+
+# Start of element C11
+ChangePinName(C11, 2, 2)
+ChangePinName(C11, 1, 1)
+
+# Start of element U2
+ChangePinName(U2, 3, IN+)
+ChangePinName(U2, 5, NULL)
+ChangePinName(U2, 7, +V)
+ChangePinName(U2, 1, NULL)
+ChangePinName(U2, 8, N.C.)
+ChangePinName(U2, 6, OUT)
+ChangePinName(U2, 4, V-)
+ChangePinName(U2, 2, IN-)
+
+# Start of element CONN1
+ChangePinName(CONN1, 5, 5)
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 2, 2)
+
+# Start of element C31
+ChangePinName(C31, 2, 2)
+ChangePinName(C31, 1, 1)
+
+# Start of element R13
+ChangePinName(R13, 1, 1)
+ChangePinName(R13, 2, 2)
+
+# Start of element R11
+ChangePinName(R11, 1, 1)
+ChangePinName(R11, 2, 2)
+
+# Start of element S1
+ChangePinName(S1, 8, 8)
+ChangePinName(S1, 7, 7)
+ChangePinName(S1, 3, 3)
+ChangePinName(S1, 2, 2)
+ChangePinName(S1, 1, 1)
+ChangePinName(S1, 6, 6)
+ChangePinName(S1, 5, 5)
+ChangePinName(S1, 4, 4)
+
+# Start of element R10
+ChangePinName(R10, 1, 1)
+ChangePinName(R10, 2, 2)
+
+# Start of element CONN4
+ChangePinName(CONN4, 5, 5)
+ChangePinName(CONN4, 3, 3)
+ChangePinName(CONN4, 1, 1)
+ChangePinName(CONN4, 4, 4)
+ChangePinName(CONN4, 2, 2)
+
+# Start of element C10
+ChangePinName(C10, 2, 2)
+ChangePinName(C10, 1, 1)
+
+# Start of element C12
+ChangePinName(C12, 2, 2)
+ChangePinName(C12, 1, 1)
+
+# Start of element C13
+ChangePinName(C13, 2, 2)
+ChangePinName(C13, 1, 1)
+
+# Start of element CONN3
+ChangePinName(CONN3, 4, 4)
+ChangePinName(CONN3, 3, 3)
+ChangePinName(CONN3, 1, 1)
+ChangePinName(CONN3, 2, 2)
+
+# Start of element CONN2
+ChangePinName(CONN2, 8, 8)
+ChangePinName(CONN2, 7, 7)
+ChangePinName(CONN2, 6, 6)
+ChangePinName(CONN2, 5, 5)
+ChangePinName(CONN2, 3, 3)
+ChangePinName(CONN2, 1, 1)
+ChangePinName(CONN2, 4, 4)
+ChangePinName(CONN2, 2, 2)
+
+# Start of element R31
+ChangePinName(R31, 1, 1)
+ChangePinName(R31, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element C7
+ChangePinName(C7, 2, -)
+ChangePinName(C7, 1, +)
+
+# Start of element R24
+ChangePinName(R24, 1, 1)
+ChangePinName(R24, 2, 2)
+
+# Start of element C6
+ChangePinName(C6, 2, -)
+ChangePinName(C6, 1, +)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element R23
+ChangePinName(R23, 1, 1)
+ChangePinName(R23, 2, 2)
+
+# Start of element R22
+ChangePinName(R22, 1, 1)
+ChangePinName(R22, 2, 2)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element VREG1
+ChangePinName(VREG1, 1, Adjust)
+ChangePinName(VREG1, 3, Vin)
+ChangePinName(VREG1, 2, Vout)
---
+++ mic amp/oshpark/osh.sh
@@ -0,0 +1,11 @@
+rm MicAmp_v1.fab.gbr
+mv MicAmp_v1.bottom.gbr MicAmp_v1.gbl
+mv MicAmp_v1.bottommask.gbr MicAmp_v1.gbs
+mv MicAmp_v1.bottomsilk.gbr MicAmp_v1.gbo
+mv MicAmp_v1.outline.gbr MicAmp_v1.gko
+mv MicAmp_v1.plated-drill.cnc MicAmp_v1.xln
+mv MicAmp_v1.top.gbr MicAmp_v1.gtl
+mv MicAmp_v1.topmask.gbr MicAmp_v1.gts
+mv MicAmp_v1.topsilk.gbr MicAmp_v1.gto
+
+
