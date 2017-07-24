3d91943c3798f8d1dae67bddf5bacd82b5943317
Evan Foss <evanfoss@gmail.com>
2017-07-24 16:52:05 -0400

    1st real commit

---
+++ strain gauge amp/MicAmp_v1.cmd
@@ -0,0 +1,140 @@
+# Pin name action command file
+
+# Start of element C14
+ChangePinName(C14, 2, 2)
+ChangePinName(C14, 1, 1)
+
+# Start of element C13
+ChangePinName(C13, 2, 2)
+ChangePinName(C13, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element C7
+ChangePinName(C7, 2, 2)
+ChangePinName(C7, 1, 1)
+
+# Start of element C8
+ChangePinName(C8, 2, 2)
+ChangePinName(C8, 1, 1)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, -)
+ChangePinName(C2, 1, +)
+
+# Start of element U1
+ChangePinName(U1, 3, +Vin)
+ChangePinName(U1, 5, Inull1)
+ChangePinName(U1, 4, Inull)
+ChangePinName(U1, 7, -Vs)
+ChangePinName(U1, 6, Out)
+ChangePinName(U1, 8, RG2)
+ChangePinName(U1, 2, -Vin)
+ChangePinName(U1, 1, RG1)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element VREG1
+ChangePinName(VREG1, 4, BYP)
+ChangePinName(VREG1, 3, \_SHDN\_)
+ChangePinName(VREG1, 5, +Vout)
+ChangePinName(VREG1, 2, GND)
+ChangePinName(VREG1, 1, +Vin)
+
+# Start of element C12
+ChangePinName(C12, 2, 2)
+ChangePinName(C12, 1, 1)
+
+# Start of element U2
+ChangePinName(U2, 3, IN+)
+ChangePinName(U2, 5, NULL)
+ChangePinName(U2, 7, +V)
+ChangePinName(U2, 1, NULL)
+ChangePinName(U2, 8, N.C.)
+ChangePinName(U2, 6, OUT)
+ChangePinName(U2, 4, V-)
+ChangePinName(U2, 2, IN-)
+
+# Start of element CONN1
+ChangePinName(CONN1, 5, 5)
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element S1
+ChangePinName(S1, 8, 8)
+ChangePinName(S1, 7, 7)
+ChangePinName(S1, 3, 3)
+ChangePinName(S1, 2, 2)
+ChangePinName(S1, 1, 1)
+ChangePinName(S1, 6, 6)
+ChangePinName(S1, 5, 5)
+ChangePinName(S1, 4, 4)
+
+# Start of element CONN4
+ChangePinName(CONN4, 5, 5)
+ChangePinName(CONN4, 3, 3)
+ChangePinName(CONN4, 1, 1)
+ChangePinName(CONN4, 4, 4)
+ChangePinName(CONN4, 2, 2)
+
+# Start of element C11
+ChangePinName(C11, 2, 2)
+ChangePinName(C11, 1, 1)
+
+# Start of element C9
+ChangePinName(C9, 2, 2)
+ChangePinName(C9, 1, 1)
+
+# Start of element C10
+ChangePinName(C10, 2, 2)
+ChangePinName(C10, 1, 1)
+
+# Start of element CONN3
+ChangePinName(CONN3, 4, 4)
+ChangePinName(CONN3, 3, 3)
+ChangePinName(CONN3, 1, 1)
+ChangePinName(CONN3, 2, 2)
+
+# Start of element CONN2
+ChangePinName(CONN2, 8, 8)
+ChangePinName(CONN2, 7, 7)
+ChangePinName(CONN2, 6, 6)
+ChangePinName(CONN2, 5, 5)
+ChangePinName(CONN2, 3, 3)
+ChangePinName(CONN2, 1, 1)
+ChangePinName(CONN2, 4, 4)
+ChangePinName(CONN2, 2, 2)
+
+# Start of element C5
+ChangePinName(C5, 2, -)
+ChangePinName(C5, 1, +)
+
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
